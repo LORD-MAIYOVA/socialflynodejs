3e252d577d0c99db2e66cd5fd6e8ebcdfaf8885d
LORD MAIYOVA <bhaiyakaku18@gmail.com>
2022-01-19 15:08:45 +0530

    Initial commit

---
+++ routes/message.js
@@ -0,0 +1,33 @@
+
+const router = require("express").Router();
+
+const Message = require("../models/Message");
+
+// post a message
+
+router.post('/', async(req,res)=>{
+    const newMessage = new Message(req.body)
+
+    try {
+       const saveMessage = await newMessage.save();
+       res.status(200).json(saveMessage);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// get a message
+
+ router.get('/:conversationId', async(req,res)=>{
+     try {
+        const messages = await Message.find({
+            conversationId: req.params.conversationId,
+          });
+        res.status(200).json(messages);
+     } catch (error) {
+        res.status(500).json(error)
+     }
+
+ })
+
+ module.exports = router;
